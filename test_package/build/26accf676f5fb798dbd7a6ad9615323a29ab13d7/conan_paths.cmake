0c58eaee67c7de360a6ccaed3f30ea838e0b8291
Mark Jan van Kampen <mjvk@allseas.com>
2019-08-12 14:36:58 +0200

    initial commit try 2

--- test_package/build/26accf676f5fb798dbd7a6ad9615323a29ab13d7/conan_paths.cmake
+++
@@ -1,3 +0,0 @@
-set(CONAN_GCOVR_INSTALLER_ROOT "C:/Users/mjvk/.conan/data/gcovr_installer/4.1/mjvk/testing/package/456f15897172eef340fcbac8a70811f2beb26a93")
-set(CMAKE_MODULE_PATH "C:/Users/mjvk/.conan/data/gcovr_installer/4.1/mjvk/testing/package/456f15897172eef340fcbac8a70811f2beb26a93/" ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
-set(CMAKE_PREFIX_PATH "C:/Users/mjvk/.conan/data/gcovr_installer/4.1/mjvk/testing/package/456f15897172eef340fcbac8a70811f2beb26a93/" ${CMAKE_PREFIX_PATH} ${CMAKE_CURRENT_LIST_DIR})
--- test_package/build/55e16be68655abeb61cac85f1b138237d002b8b2/conan_paths.cmake
+++
@@ -1,6 +0,0 @@
-set(CONAN_NEUTRINO_TOOLCHAIN_INSTALLER_ROOT "C:/Users/mjvk/.conan/data/neutrino_toolchain_installer/0.1/mjvk/testing/package/7db8e31913cb249c780428805485e962facb0366")
-set(CONAN_GCOVR_INSTALLER_ROOT "C:/Users/mjvk/.conan/data/gcovr_installer/4.1/mjvk/testing/package/456f15897172eef340fcbac8a70811f2beb26a93")
-set(CMAKE_MODULE_PATH "C:/Users/mjvk/.conan/data/neutrino_toolchain_installer/0.1/mjvk/testing/package/7db8e31913cb249c780428805485e962facb0366/"
-			"C:/Users/mjvk/.conan/data/gcovr_installer/4.1/mjvk/testing/package/456f15897172eef340fcbac8a70811f2beb26a93/" ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
-set(CMAKE_PREFIX_PATH "C:/Users/mjvk/.conan/data/neutrino_toolchain_installer/0.1/mjvk/testing/package/7db8e31913cb249c780428805485e962facb0366/"
-			"C:/Users/mjvk/.conan/data/gcovr_installer/4.1/mjvk/testing/package/456f15897172eef340fcbac8a70811f2beb26a93/" ${CMAKE_PREFIX_PATH} ${CMAKE_CURRENT_LIST_DIR})
